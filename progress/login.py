b4d2a87591296e566a544a9f9887dbae46c6894b
dabin <1073889058@qq.com>
2016-10-12 22:49:27 +0800

    remodified login

--- progress/login.py
+++ progress/login.py
@@ -8,3 +8,16 @@ class LoginHelper:
 		self.cookie = cookielib.LWPCookieJar()
 		self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.cookie))
 		urllib2.install_opener(self.opener)
+
+	def login(self):
+		data = {
+			'email' : self.username
+			'password' : self.password
+		}
+		loginUrl = 'http://www.renren.com/ajaxLogin/login?1=1&uniqueTimestamp=2016932127568'
+		request = urllib2.Request(loginUrl, urllib.urlencode(data))
+		try:
+			response = self.opener.open(request)
+			usr = response.geturl()
+			print url
+		
